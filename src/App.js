36941aa62ecaa32bd628fb3940dc9319bd4dfa5c
Raj Ranjan <raj99391435@gmail.com>
2021-09-03 17:04:00 +0530

    new-firebase-react

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import React, { useState, useEffect, Fragment} from "react";
+import React, { useState, useEffect} from "react";
 import analytics from "./firebase";
-
-
+  
 function App() {
-  const [users, setusers] =useState([])
+  const [users, setusers] =useState([]);
 
   useEffect(() => {
-    const fetchdata = async() => {
+    const fetchuser = async() => {
       const db = analytics.firestore()
       const data = await db.collection("users").get()
       setusers(data.docs.map(doc => doc.data()))
     }
-    fetchdata()
+    fetchuser()
   }, [])
 
   return(
-   <Fragment>
       <ul>
-      {users.map(user => {
-        return(
+      {users.map(user => (
         <li key ={user.name}>{user.name}</li>
-        )
-      })}
+      ))}
       </ul>
-    </Fragment>
     
   );
 }
@@ -31,54 +26,3 @@ function App() {
 export default App;
 
 
-
-
-
-
-
-
-
-
-
-
-// import { useState, React, Fragment} from "react";
-// import UsersList from "./component/UsersList";
-
-// function App() {
-//   const [users, setUsers] =useState([]);
-//   const [loading, setisLoading] =useState(false);
-
-//   async function fetchUserhandler() {
-//     setisLoading(true);
-    
-//     const response = await fetch('https://console.firebase.google.com/u/0/project/speakapp-d4970/firestore/data/~2Fusers/myusers.json');
-//     const data = response.json();
-
-//     const transformedUsers = data.map((user) => {
-//       return {
-//         name: user.name,
-//         phone: user.phone,
-//       };
-//     });
-    
-//     setUsers(transformedUsers);
-    
-//     setisLoading(false);
-//   }
-
-//   return(
-//    <Fragment>
-//      <section>
-//        <button onClick={fetchUserhandler}>get usets</button>
-//      </section>
-//      <section>
-//        {!loading && users.length > 0 && <UsersList users={users} />}
-//        {!loading && users.length === 0 && <p>Found no user</p>}
-//        {!loading && <p>Loading...</p>}
-//      </section>
-//     </Fragment>
-    
-//   );
-// };
-
-// export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,37 +1,21 @@
-// // import * from './google-services.json';
 
-// // const functions = require("firebase");
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
 
-// // const admin = require("firebase-admin");
-// // const firebaseapp =admin.initializeApp();
-
-// // const firebase= firestore.firebaseapp();
-
-// // export default firebase;
-// import "@google-cloud/firestore"
-//  var admin = require("firebase-admin");
-
-
-//  var serviceAccount = require("path/to/serviceAccountKey.json");
-
-//  admin.initializeApp({
-//    credential: admin.credential.cert(serviceAccount)
-//  });
-
-//  export default serviceAccount;
-
-import { initializeApp} from 'firebase/app';
-import {getAnalytics} from "firebase/analytics";
-
- const config = {
-     apiKey: "AIzaSyDVwn-aUw7HmO6FLIvn58YYfC1dvT_EmFI",
-     authDomain: "firebase-adminsdk-ww672@speakapp-d4970.iam.gserviceaccount.com",
-     projectId: "speakapp-d4970",
-     storageId: "speakapp-d4970.appspot.com",
-     messagingSenderId: "82803959843",
-     appId: "1:82803959843:android:4c3f124089183c986a6996"
- };
- const app= initializeApp(config);
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDVwn-aUw7HmO6FLIvn58YYfC1dvT_EmFI",
+  authDomain: "speakapp-d4970.firebaseapp.com",
+  projectId: "speakapp-d4970",
+  storageBucket: "speakapp-d4970.appspot.com",
+  messagingSenderId: "82803959843",
+  appId: "1:82803959843:web:0cb499d4a03510346a6996",
+  measurementId: "G-EMQNSWP8RS"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
 export default analytics;
