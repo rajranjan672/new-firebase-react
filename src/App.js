3e605abbff4f7c85c47ac53acef04a0d3f71adb9
Raj Ranjan <raj99391435@gmail.com>
2021-09-05 13:22:58 +0530

    rctfb

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     <div>
       {users.map((user) => (
         <li key ={user.name}>{user.name}
-        -{user.phone} - {user.email} - {user.gender} - {user.minleft}"min"-"on call"-{user.onCall} -"isOnline" -{user.isOnline} - </li>
+        -{user.phone} - {user.email} - {user.gender} - {user.country} -{user.minsleft}"min"-"on call"-{user.onCall} -"isOnline" -{user.isOnline} - </li>
       ))}
     </div>
     
