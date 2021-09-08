4b469dfe60e1fca56b2e329f50242c9ac575bd28
Raj Ranjan <raj99391435@gmail.com>
2021-09-08 14:16:51 +0530

    raj.new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { collection, getDocs } from "@firebase/firestore";
 
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect, Fragment} from "react";
+
 import db from "./firebase";
 
 function App() {
@@ -13,15 +14,43 @@ function App() {
         setusers(userSnapshot.docs.map((doc) => doc.data()));
     })();
 }, []);
-
+  console.log(users)
   return(
+    <Fragment>
     <div>
-      {users.map((user) => (
-        <li key ={user.name}><span style={{fontWeight: 'bold'}}>{user.name}</span>
-         = <span style={{color:'#F60BD5'}}>{user.country}</span> , <span style={{color:'blue'}}>{user.phone}</span> , <span style={{color:'red'}}>{user.email}</span> , <span style={{color:user.gender.Male? '#26CCCF': '#EE44F7'}}>{user.gender} </span> ,  {user.minsleft} "min-left" , "on call" =<span style={{color:user.onCall? 'green': 'red'}}>{user.onCall ? "true": "false"}</span> , "isOnline" =<span style={{color: user.isOnline? '#26CF29': '#F20D35'}}>{user.isOnline ? "true": "false"}</span>  </li>
-      ))}
+      <h1 style={{textAlign:'center', color:''}}>Users Details</h1>
     </div>
     
+    <div >
+      <table border="1px" position='absolute'>
+      <thead>
+      <tr>
+      <th style={{color: '#1350d4',fontWeight:'bold', backgroundColor:'#dbe6cc', columnWidth:'120px'}}>Name</th>
+      <th style={{backgroundColor:'#dbe6cc', columnWidth:'180px'}}>Country</th>
+      <th style={{backgroundColor:'#dbe6cc', columnWidth:'180px'}}>Mobile</th>
+      <th style={{color: '#1350d4',fontWeight:'bold',backgroundColor:'#dbe6cc', columnWidth:'300px'}}>email</th>
+      <th style={{backgroundColor:'#dbe6cc',columnWidth:'145px'}}>Gender</th>
+      <th style={{backgroundColor:'#dbe6cc',columnWidth:'120px'}}>isOnline</th>
+      <th style={{backgroundColor:'#dbe6cc', columnWidth:'120px'}}>onCall</th>
+      <th style={{backgroundColor:'#dbe6cc', columnWidth:'150px'}}>minsleft</th>
+
+      </tr>
+      </thead>
+      {users.map((user) => (
+        <tr>
+        <td style={{color: '#1350d4', textAlign:"center"}}>{user.name}</td>
+        <td style={{textAlign: 'center'}}>{user.country}</td>
+        <td style={{textAlign: 'center'}}>{user.phone}</td>
+        <td style={{color: '#1350d4', textAlign:"center"}}>{user.email}</td>
+        <td style={{marginLeft:'100px', textAlign:"center"}}>{user.gender}</td>
+        <td style={{color:user.isOnline ?'#1ebd2e': '#e62412', fontWeight:'bold', textAlign:"center"}}>{user.isOnline ?"true": "false"}</td>
+        <td style={{color:user.onCall? '#1ebd2e' :'#e62412', fontWeight:'bold', textAlign:"center"}}>{user.onCall? "true": "false"}</td>
+        <td style={{textAlign: 'center'}}>{user.minsleft}</td>
+        </tr>
+      ))}
+      </table>
+    </div>
+    </Fragment>
   );
 }
 export default App;
