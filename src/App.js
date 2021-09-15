2956ac2ff660718b174717b3317281b4e33de0b7
Raj Ranjan <raj99391435@gmail.com>
2021-09-15 19:16:00 +0530

    raj.new.hover

--- src/App.js
+++ src/App.js
@@ -27,25 +27,26 @@ function App() {
       <tr>
       <th style={{color: '#1350d4',fontWeight:'bold', backgroundColor:'#dbe6cc', columnWidth:'120px'}}>Name</th>
       <th style={{backgroundColor:'#dbe6cc', columnWidth:'180px'}}>Country</th>
-      <th style={{backgroundColor:'#dbe6cc', columnWidth:'180px'}}>Mobile</th>
-      <th style={{color: '#1350d4',fontWeight:'bold',backgroundColor:'#dbe6cc', columnWidth:'300px'}}>email</th>
+      <th className="heading" data-hover="varified" style={{backgroundColor:'#dbe6cc', columnWidth:'180px', cursor:'pointer'}}>Mobile</th>
+      <th className="heading" data-hover="varified" style={{color: '#1350d4',fontWeight:'bold',backgroundColor:'#dbe6cc', columnWidth:'300px', cursor:'pointer'}}>email</th>
       <th style={{backgroundColor:'#dbe6cc',columnWidth:'145px'}}>Gender</th>
       <th style={{backgroundColor:'#dbe6cc',columnWidth:'120px'}}>isOnline</th>
       <th style={{backgroundColor:'#dbe6cc', columnWidth:'120px'}}>onCall</th>
       <th style={{backgroundColor:'#dbe6cc', columnWidth:'150px'}}>minsleft</th>
+      
 
       </tr>
       </thead>
       {users.map((user) => (
         <tr>
-        <td className="box" style={{color: '#1350d4', textAlign:"center", cursor:'pointer'}} >{user.name}</td>
-        <td className="box" style={{textAlign: 'center', cursor:'pointer'}}>{user.country}</td>
-        <td className="box" style={{textAlign: 'center', cursor:'pointer'}}>{user.phone}</td>
-        <td className="box" style={{color: '#1350d4', textAlign:"center", cursor:'pointer'}}>{user.email}</td>
-        <td className="box" style={{marginLeft:'100px', textAlign:"center", cursor:'pointer'}}>{user.gender}</td>
-        <td className="box" style={{color:user.isOnline ?'#1ebd2e': '#e62412', fontWeight:'bold', textAlign:"center", cursor:'pointer'}}>{user.isOnline ?"true": "false"}</td>
-        <td className="box" style={{color:user.onCall? '#1ebd2e' :'#e62412', fontWeight:'bold', textAlign:"center", cursor:'pointer'}}>{user.onCall? "true": "false"}</td>
-        <td className="box" style={{textAlign: 'center', cursor:'pointer'}}>{user.minsleft}</td>
+        <td className="box body" style={{color: '#1350d4', textAlign:"center", cursor:'pointer'}} >{user.name}</td>
+        <td className="box body" style={{textAlign: 'center', cursor:'pointer'}}>{user.country}</td>
+        <td className="box body" style={{textAlign: 'center', cursor:'pointer'}}>{user.phone}</td>
+        <td className="box body" style={{color: '#1350d4', textAlign:"center", cursor:'pointer'}}>{user.email}</td>
+        <td className="box body" style={{marginLeft:'100px', textAlign:"center", cursor:'pointer'}}>{user.gender}</td>
+        <td className="box body" style={{color:user.isOnline ?'#1ebd2e': '#e62412', fontWeight:'bold', textAlign:"center", cursor:'pointer'}}>{user.isOnline ?"true": "false"}</td>
+        <td className="box body" style={{color:user.onCall? '#1ebd2e' :'#e62412', fontWeight:'bold', textAlign:"center", cursor:'pointer'}}>{user.onCall? "true": "false"}</td>
+        <td className="box body" style={{textAlign: 'center', cursor:'pointer'}}>{user.minsleft}</td>
         </tr>
       ))}
       </table>
